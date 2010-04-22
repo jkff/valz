1e254495305f7e8d0b7d2f054e9dc52e34e1f351
slonpts <slonpts@gmail.com>
2010-04-23 02:39:30 +0400

    H2DataStore now use connection pool Created test for H2DataStore.

--- server/src/java/org/valz/server/H2DataStore.java
+++ server/src/java/org/valz/server/H2DataStore.java
@@ -2,16 +2,25 @@ package org.valz.server;
 
 import flexjson.JSONDeserializer;
 import flexjson.JSONSerializer;
+import org.apache.commons.dbcp.ConnectionFactory;
+import org.apache.commons.dbcp.DriverManagerConnectionFactory;
+import org.apache.commons.dbcp.PoolableConnectionFactory;
+import org.apache.commons.dbcp.PoolingDataSource;
+import org.apache.commons.pool.ObjectPool;
+import org.apache.commons.pool.impl.GenericObjectPool;
 import org.valz.util.aggregates.Aggregate;
 
-
-import java.sql.*;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 
 public class H2DataStore implements DataStore {
 
-    private final Connection conn;
+    private final DataSource dataSource;
 
 
 
@@ -21,105 +30,103 @@ public class H2DataStore implements DataStore {
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
-        try {
-            conn = DriverManager.getConnection(String.format("jdbc:h2:%s", filename), "sa", "");
-            conn.createStatement().execute("CREATE TABLE IF NOT EXISTS Aggregates ( name varchar PRIMARY KEY, aggregate varchar, value varchar);");
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+
+        ObjectPool connectionPool = new GenericObjectPool(null);
+        ConnectionFactory connectionFactory =
+                new DriverManagerConnectionFactory(String.format("jdbc:h2:%s", filename), null);
+        PoolableConnectionFactory poolableConnectionFactory =
+                new PoolableConnectionFactory(connectionFactory, connectionPool, null, null, false, true);
+        dataSource = new PoolingDataSource(connectionPool);
+
+        execute("CREATE TABLE IF NOT EXISTS Aggregates ( name varchar PRIMARY KEY, aggregate varchar, value varchar);");
     }
 
 
 
     public void createAggregate(String name, Aggregate<?> aggregate, Object value) {
-        execute("INSERT INTO Aggregates(name, aggregate, value) VALUES(?, ?, ?);",
-                name,
-                new JSONSerializer().serialize(aggregate),
-                new JSONSerializer().serialize(value));
+        execute("INSERT INTO Aggregates(name, aggregate, value) VALUES(?, ?, ?);", name,
+                new JSONSerializer().serialize(aggregate), new JSONSerializer().serialize(value));
     }
 
     public Collection<String> listVars() {
-        return executeQuery(
-            new Function<Collection<String>>() {
-                public Collection<String> apply(ResultSet resultSet) throws SQLException {
-                    Collection<String> list = new ArrayList<String>();
-                    while (resultSet.next()) {
-                        list.add(resultSet.getString(1));
-                    }
-                    return list;
+        return executeQuery(new Function<Collection<String>>() {
+            public Collection<String> apply(ResultSet resultSet) throws SQLException {
+                Collection<String> list = new ArrayList<String>();
+                while (resultSet.next()) {
+                    list.add(resultSet.getString(1));
                 }
-            },
-            "SELECT name FROM Aggregates;");
+                return list;
+            }
+        }, "SELECT name FROM Aggregates;");
     }
 
     public Aggregate getAggregate(String name) {
-        return executeQuery(
-            new Function<Aggregate>() {
-                public Aggregate apply(ResultSet resultSet) throws SQLException {
-                    if (!resultSet.next()) {
-                        return null;
-                    }
-                    String str = resultSet.getString(1);
-                    return new JSONDeserializer<Aggregate>().deserialize(str);
+        return executeQuery(new Function<Aggregate>() {
+            public Aggregate apply(ResultSet resultSet) throws SQLException {
+                if (!resultSet.next()) {
+                    return null;
                 }
-            },
-            "SELECT aggregate FROM Aggregates WHERE name = ?;",
-                name);
+                String str = resultSet.getString(1);
+                return new JSONDeserializer<Aggregate>().deserialize(str);
+            }
+        }, "SELECT aggregate FROM Aggregates WHERE name = ?;", name);
     }
 
     public Object getValue(String name) {
-        return executeQuery(
-            new Function<Object>() {
-                public Object apply(ResultSet resultSet) throws SQLException {
-                    if (!resultSet.next()) {
-                        return null;
-                    }
-                    String str = resultSet.getString(1);
-                    return new JSONDeserializer().deserialize(str);
+        return executeQuery(new Function<Object>() {
+            public Object apply(ResultSet resultSet) throws SQLException {
+                if (!resultSet.next()) {
+                    return null;
                 }
-            },
-            "SELECT value FROM Aggregates WHERE name = ?;", name);
+                String str = resultSet.getString(1);
+                return new JSONDeserializer().deserialize(str);
+            }
+        }, "SELECT value FROM Aggregates WHERE name = ?;", name);
     }
 
     public void setValue(String name, Object value) {
-        execute("UPDATE Aggregates SET value = ? WHERE name = ?;",
-                new JSONSerializer().serialize(value),
-                name);
+        execute("UPDATE Aggregates SET value = ? WHERE name = ?;", new JSONSerializer().serialize(value), name);
     }
 
 
     private <T> T executeQuery(Function<T> func, String query, String... params) {
-        ResultSet resultSet = null;
+        Connection conn = null;
+        PreparedStatement statement = null;
         try {
-            PreparedStatement statement = conn.prepareStatement(query);
-            for (int i=0; i<params.length; i++) {
+            conn = dataSource.getConnection();
+            statement = conn.prepareStatement(query);
+            for (int i = 0; i < params.length; i++) {
                 statement.setString(i + 1, params[i]);
             }
-            resultSet = statement.executeQuery();
-            return func.apply(resultSet);
+            if (func == null) {
+                statement.execute();
+                return null;
+            } else {
+                ResultSet resultSet = statement.executeQuery();
+                return func.apply(resultSet);
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         } finally {
             try {
-                if (resultSet != null) {
-                    resultSet.close();
+                if (statement != null) {
+                    statement.close();
                 }
-            } catch(SQLException e) {
+            } catch (SQLException e) {
+                // Ignore
+            }
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
                 // Ignore
             }
         }
     }
 
     private void execute(String query, String... params) {
-        try {
-            PreparedStatement statement = conn.prepareStatement(query);
-            for (int i=0; i<params.length; i++) {
-                statement.setString(i + 1, params[i]);
-            }
-            statement.execute();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        executeQuery(null, query, params);
     }
 
     private interface Function<T> {
