de1c2bedda8fa291fc4dd26f7d23e51f71201eb5
Eugene Kirpichov <ekirpichov@gmail.com>
2010-07-16 17:03:52 +0400

    Forgotten to add some files.

---
+++ web-ui/src/java/org/valz/viewer/ViewerInternalConfig.java
@@ -0,0 +1,25 @@
+package org.valz.viewer;
+
+import org.valz.aggregates.AggregateRegistry;
+import org.valz.backends.ReadBackend;
+import org.valz.backends.RemoteReadBackend;
+import org.valz.keytypes.KeyTypeRegistry;
+
+import java.util.List;
+
+public class ViewerInternalConfig {
+    public final ReadBackend readBackend;
+    public final AggregateRegistry aggregateRegistry;
+
+    public ViewerInternalConfig(ReadBackend readBackend, AggregateRegistry aggregateRegistry) {
+        this.readBackend = readBackend;
+        this.aggregateRegistry = aggregateRegistry;
+    }
+
+    public static ViewerInternalConfig getConfig(List<String> urls, int chunkSize) {
+        KeyTypeRegistry keyTypeRegistry = KeyTypeRegistry.create();
+        AggregateRegistry aggregateRegistry = AggregateRegistry.create();
+        ReadBackend readBackend = new RemoteReadBackend(urls, keyTypeRegistry, aggregateRegistry, chunkSize);
+        return new ViewerInternalConfig(readBackend, aggregateRegistry);
+    }
+}
