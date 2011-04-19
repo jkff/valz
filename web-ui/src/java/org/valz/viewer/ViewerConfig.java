7e4a6a83b321cfd76cb02b2c8ab6ab9a0b745d71
Gleb Smirnov <gsmir@yandex-team.ru>
2011-04-19 20:14:54 +0400

    Added default URLs to ViewerConfig and fixed incorrect reading of URLs

--- web-ui/src/java/org/valz/viewer/ViewerConfig.java
+++ web-ui/src/java/org/valz/viewer/ViewerConfig.java
@@ -5,6 +5,8 @@ import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
 public class ViewerConfig {
+	
+	public static final String DEFAULT_URL = "http://localhost:9125";
 
     public final List<String> urls;
 
@@ -15,11 +17,12 @@ public class ViewerConfig {
     public static ViewerConfig read() {
         Preferences prefs = Preferences.userRoot().node("server.config");
         Preferences urlPrefs = prefs.node("urls");
+        urlPrefs.node("default").put("defaultUrl", DEFAULT_URL);
 
         Set<String> urls = new HashSet<String>();
         try {
             for (String key : urlPrefs.childrenNames()) {
-                urls.add(urlPrefs.get("key", "http://localhost:8080"));
+                urls.add(urlPrefs.get(key, "http://localhost:9125"));
             }
         } catch (BackingStoreException e) {
             // Ignore
